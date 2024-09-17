53f4fc7bc8850e5ff94367fb73809e54f99cc755
Yancap <yangabriel3001f@gmail.com>
2024-09-17 11:11:03 -0300

    client_id

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  client_id: 'Ov23liCeHvH2TP67FU5m',
+  client_id: 'f53b920f83c565d04e86',
   STRIPE_PUBLIC_KEY: 'pk_test_51N8SKYCGnWxAsuIlx2sRDB4SjsOBqJdrVixIl0HzITgVyWfl5lhIdGnBSz6btMRIpuxKtDHB6pDUeVNco6jPf7YN00Hp7BaYOw'
 
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
   production: true,
-  client_id: '', // configurar para PROD,
+  client_id: 'f53b920f83c565d04e86', // configurar para PROD,
   STRIPE_PUBLIC_KEY: 'pk_test_51N8SKYCGnWxAsuIlx2sRDB4SjsOBqJdrVixIl0HzITgVyWfl5lhIdGnBSz6btMRIpuxKtDHB6pDUeVNco6jPf7YN00Hp7BaYOw'
 };
