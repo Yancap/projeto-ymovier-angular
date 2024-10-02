27d5e922607eac08ed14f2190fc41270b7668b70
Yancap <yangabriel3001f@gmail.com>
2024-10-02 12:21:58 -0300

    ajuste

--- server/services/fauna.ts
+++ server/services/fauna.ts
@@ -1,8 +1,8 @@
 import { Client } from 'faunadb';
 import { config, DotenvParseOutput } from 'dotenv';
 
-let dotenv = config().parsed as DotenvParseOutput;
+let dotenv = (config().parsed && process.env) as DotenvParseOutput;
 
 export const fauna = new Client({
-    secret: (dotenv['FAUNADB_KEY'] && process.env) as string
+    secret: dotenv['FAUNADB_KEY']  as string
 })
